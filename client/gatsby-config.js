9869c51d39130bb9ddce2e5b4ae48f5bffec6a6c
David <dhalsey212@gmail.com>
2020-06-07 22:49:11 -0400

    [david-1] clean up 2 & run yarn

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -1,8 +1,5 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,13 +15,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Woodside on the Move`,
+        short_name: `wotm`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
