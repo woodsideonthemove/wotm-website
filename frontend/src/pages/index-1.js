deabbcf39800e2148fb9059719513853da4d1019
David <dhalsey212@gmail.com>
2020-06-07 20:24:47 -0400

    [david-1] Import Gatsby setup/reference files, static files & top level components

--- frontend/src/pages/index-1.js
+++ frontend/src/pages/index-1.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Router} from 'react-router-dom'
 import {Provider} from 'react-redux'
 import history from './history'
 import App from './app'
